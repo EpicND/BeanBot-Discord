87e4b09cf432f45eda2568292139d2f092ede737
ccans <55026208+ccans@users.noreply.github.com>
2020-05-23 17:49:17 -0500

    ok gamer

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const config = require('config.json')
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Nice name nerd, ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('ok nerd');
+  }
+});
+
+client.login('NzEzODgxMjg1MTA5NzQzNzE3.XsmlAQ._Xbju_KD4SXlSwm08ZH5cGlTcJw');
