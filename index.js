8686225c65db023ceb7b7cf23c4231645f935d81
DhairyaG23 <dhairyag23@gmail.com>
2020-05-23 18:42:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 
-const PREFIX = '!';
+const PREFIX = 'b!';
 
 client.on('ready', () => {
   console.log(`Nice name nerd, ${client.user.tag}!`);
@@ -11,29 +11,36 @@ client.on('ready', () => {
 
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('ok nerd');
+//   if (msg.content === 'ping') {
+ 
+//   }
 
-    var time1 = msg.createdTimestamp
-    msg.channel.send("Pinging...").then(lol => {
-      var time2 = lol.createdTimestamp
-      lol.edit(":ping_pong: **Pong!** **Response Time:** `" +(time2-time1)+" `Miliseconds")
-      { disableEveryone: true }
-console.log(time2-time1+" miliseconds response time")
-  })
-  }
+//   if (msg.content === 'b!fact') {
+//     msg.reply('Shut up nerd no bean facts for your broke ass');
+//   }
 
-  
+let args = msg.content.substring(PREFIX.length).split(' ');
+switch(args[0]){
+    case 'ping':
+        msg.reply('ok nerd');
+
+        var time1 = msg.createdTimestamp
+        msg.channel.send("Pinging...").then(lol => {
+          var time2 = lol.createdTimestamp
+          lol.edit(":ping_pong: **Pong!** **Response Time:** `" +(time2-time1)+" `Miliseconds")
+          { disableEveryone: true }
+          console.log(time2-time1+" miliseconds response time")
+      })
+      break;
+    case 'fact':
+        msg.reply('Shut up nerd no bean facts for your broke ass');
+        break;
+
+}
 
 
 
 });
 
-client.on('message', msg => {
-  if (msg.content === 'b!fact') {
-    msg.reply('Shut up nerd no bean facts for your broke ass');
-  }
-
-});
 
 client.login(config.token);
