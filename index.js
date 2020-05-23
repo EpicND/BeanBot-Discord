486e1e1eb8f255c7a46e25c59c0296e3e43b1864
DhairyaG23 <dhairyag23@gmail.com>
2020-05-23 18:20:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,17 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if (msg.content === 'ping') {
-    msg.reply('ok nerd');
+    // msg.reply('ok nerd');
+
+    var time1 = msg.createdTimestamp
+    msg.channel.send("Pinging...").then(lol => {
+      var time2 = lol.createdTimestamp
+      lol.edit(":ping_pong: **Pong!** **Response Time:** `" +(time2-time1)+" `Miliseconds")
+      { disableEveryone: true }
+console.log(time2-time1+" miliseconds response time")
+  })
   }
+
 });
 
 client.login(config.token);
