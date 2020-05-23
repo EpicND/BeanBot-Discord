9c79659ea345f1a1000a474a68cca677e48ad385
DhairyaG23 <dhairyag23@gmail.com>
2020-05-23 18:00:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const config = require('config.json')
+const config = require('config.json');
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
