156db6f768f4afe56639046adfc9e1366e922e5b
ccans <55026208+ccans@users.noreply.github.com>
2020-05-24 14:54:22 -0500

    ok then

---
+++ commands/info.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'info',
+    description: 'Gets the info for the bot',
+    execute(msg, args, guildSize) {
+      const infoEmbed = new Discord.MessageEmbed()
+      .setColor(0x4F7942)
+      .setAuthor("Info", "https://cdn.discordapp.com/icons/713946467877912636/433d42fd310800ee68d63bf1e5c006fd.png?size=2048")
+      // .setDescription("**'help** - Displays this message.\n**'ping** - :ping_pong: Pong! (Used to show response time)\n**'invite** - Used to invite me to your server\n**'announce** - Used by admins to address the people of the guild\n**'clear <number of messages to delete (up to 100)>** - Deletes `x` number of messages\n**'rule** - Add a rule to a channel that includes `rules`\n**'info**-Learn more about me and my owner/programmer\n**'ban** - Bans a member, type ban to learn syntax of this command\n**'kick** - Kicks a member from the guild, type 'kick for command syntax\n**'vcmute** - mutes a given member\n**'vcunmute** - unmutes a given member\n**'deafen** - Deafens a given member\n**'undeafen** - undeafens a given member\n**'warn** - Warns a member that is mentioned (Requires the MANAGE_GUILD Permission)\n**'userinfo** - Used to find the info of a certain user, usage: <'userinfo> for info about yourself <'userinfo @user> for info about certain user\n**'google** - Search google for something\n**'hastebin** - Used to hastebin to share your code!\n**'flip** - Flips a two sided coin\n**'roll** - Rolls a 6 sided dice")
+      .addField("**Bot Info**", ':b:ean bot is a funny meme bot made to fulfill your widest bean needs. Use b!help to get started. I can send bean images, bean facts, bean memes, and many other things. I am **1** day old. Many more features are coming for all of you bean addicts!'  )
+      .addField("**Developer Info**", 'This bot is made by **ccans#5709** and **DhairyaG23#7326**. We are always looking to add more features, facts, and photos. Join my server to reccomend things!')
+      .addField("**Beta**", 'BeatBot is constantly being updated!. Currently, we are working on making specific categories of beans and expanding our photo library of beans and bean memes. join our server and leave recommendations for what we should add next!')
+
+      msg.channel.send({embed: infoEmbed})
+    }
+  }
--- index.js
+++ index.js
@@ -1,67 +1,99 @@
-const fs = require('fs');
 const config = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.commands = new Discord.Collection();
-
-const prefix = 'b!';
-
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+const PREFIX = 'b!';
 
-	// set a new item in the Collection
-	// with the key as the command name and the value as the exported module
-	client.commands.set(command.name, command);
-}
+const factArray = [':B:eans are the large seeds of certain types of plants, and are technically a fruit.',
+'January 6th is National :b:ean Day. It also falls on the day in which geneticist, Gregor Mendel (:goat:), who famously used :b:ean and pea plants to test his theories on inheritance died in 1884.',
+':b:eans have been cultivated by humans for 6,000 years.',
+'In Nicaragua, newlyweds are given a bowl of :b:eans for good luck.',
+'In ancient Greece, minor public officials were elected by putting one white :b:ean with a load of black :b:eans inside a “:b:ean machine.” Whoever picked the white :b:ean got the job.',
+'An archaeologist in the 1980’s working in New Mexico came upon a clay pot sealed with pine tar containing :b:ean seeds that were 1,500 years old…and they grew!',
+':b:eans can be made into burgers, cakes, drinks, pies, fudge, muffins, jewelry, furniture (:b:ean-bag chairs!), toys, and musical instruments.',
+'In the 6th century BC, philosopher and mathematician Pythagoras (certified dumbass) had a deep philosophical dislike of :b:eans. Some historians reported his aversion was due to the belief that legumes contained the souls of the dead',
+'Approximately 71,089 people in the world have the last name :b:ean.',
+'Vermont ranks highest in searching for :b:ean recipes online. Montana and Wyoming are second and third.',
+'The longest recorded time for sitting in a bath of cold baked :b:eans is 100 hours by Barry “Captain :b:eany” Kirk',
+ ];
 
  const insultArray = [
-  "The only thing you have in common with a bean is your IQ. spell it properly nerd",
-  "I feel bad for your english teacher if you can't even spell the commands correctly",
-  "?? :clown:",
-  'Choose an actual command bimbo'
+    "The only thing you have in common with a bean is your IQ. spell it properly nerd",
+    "I feel bad for your english teacher if you can't even spell the commands correctly",
+    "?? :clown:"
+
  ];
 
 client.on('ready', () => {
-  console.log(`Nice name nerd, ${client.user.tag}!`);
+  console.log(`Nice name nerd, ${client.user.tag}! Use b!help to get started`);
 });
 
 
 client.on('message', msg => {
+//   if (msg.content === 'ping') {
 
-    if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+//   }
 
+//   if (msg.content === 'b!fact') {
+//     msg.reply('Shut up nerd no bean facts for your broke ass');
+//   }
 
-    const args = msg.content.slice(prefix.length).split(/ +/);
-    for(x=0; args[x] == '' || args[x] == ' ';){
-        args.shift();
-        // console.log('epic' + args[x])   
-    }
+let args = msg.content.substring(PREFIX.length).split(' ');
+if(msg.content.startsWith(PREFIX)){
+switch(args[0]){
+    case 'ping':
+        msg.reply('ok nerd');
 
-	const command = args.shift().toLowerCase();
-  var x = Math.floor(Math.random()*3);
-  
-  if (!client.commands.has(command)) return msg.reply(insultArray[x]);
+        var time1 = msg.createdTimestamp
+        msg.channel.send("Pinging...").then(lol => {
+          var time2 = lol.createdTimestamp
+          lol.edit(":ping_pong: **Pong!** **Response Time:** `" +(time2-time1)+" `Miliseconds")
+          { disableEveryone: true }
+          console.log(time2-time1+" miliseconds response time")
+      })
+    break;
+    // case 'fact' || 'facts':
+    //     msg.reply('Shut up nerd no bean facts for your broke ass');
+    // break;
+  case 'help':
+      msg.channel.send('maybe next year')
+        .then(lol => {
+        lol.edit("check out our discord: www.discord.com. \n b!help: get a list of commands \n b!ping: a command for nerds \n b!fact: get a (not so) fun fact about :b:eans \n b!img: get a photo of beans \n b!exp: check ur server's bean level \n b!invite: invite me to a server :flushed:")
+        { disableEveryone: true }
+    })
+    break;
+  case 'ping':
+      msg.reply('ok nerd');
 
-	try {
-		client.commands.get(command).execute(msg, args, client.guilds.size);
-	} catch (error) {
-		console.error(error);
-		msg.channel.send('there was an error trying to execute that command!');
-    }
+      var time1 = msg.createdTimestamp
+      msg.channel.send("Pinging...").then(lol => {
+        var time2 = lol.createdTimestamp
+        lol.edit(":ping_pong: **Pong!** **Response Time:** `" +(time2-time1)+" `Miliseconds")
+        { disableEveryone: true }
+        console.log(time2-time1+" miliseconds response time")
+    })
+    break;
+  case 'fact':
+      var s = Math.floor(Math.random()*6);
+      if(s > 0){
+      var x = Math.floor(Math.random() * 11);
+      msg.channel.send(factArray[x])
+      } else {
+          msg.channel.send("Shut up nerd no :b:ean facts for your broke ass")
+      }
 
-});
-
-function defaultCase(msg){
-    var x = Math.floor(Math.random()*3)
+      break;
+    default:
+      var x = Math.floor(Math.random()*2)
       msg.reply(insultArray[x]);
+      break;
 
 }
 
-var configToken;
 
-if(process.env.BOT_TOKEN){configToken = process.env.BOT_TOKEN.toString() }else {configToken = config.token};
-console.log(configToken)
-client.login(configToken);
+}
+});
+
+
+client.login(config.token);
