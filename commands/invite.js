39d23312143cf227fd34958bd215bb7f35b64985
ccans <55026208+ccans@users.noreply.github.com>
2020-05-24 15:34:41 -0500

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -3,6 +3,6 @@ module.exports = {
     name: 'invite',
     description: 'Invite the bot to your server',
     execute(msg, args) {
-      msg.channel.send("Use this link to add me bot to your server! \n https://discord.com/api/oauth2/authorize?client_id=713881285109743717&permissions=8&scope=bot");
+      msg.channel.send("Use this link to add me to your server! \n https://discord.com/api/oauth2/authorize?client_id=713881285109743717&permissions=8&scope=bot");
     }
   }
