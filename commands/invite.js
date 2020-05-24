1e019aa4f455678a98af465bbd8156e227f46a8e
DhairyaG23 <dhairyag23@gmail.com>
2020-05-24 14:53:18 -0500

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -3,6 +3,6 @@ module.exports = {
     name: 'invite',
     description: 'Invite the bot to your server',
     execute(msg, args) {
-      msg.reply("Use this link to add our bot to your server! \n https://discord.com/api/oauth2/authorize?client_id=713881285109743717&permissions=67584&scope=bot");
+      msg.channel.send("Use this link to add me bot to your server! \n https://discord.com/api/oauth2/authorize?client_id=713881285109743717&permissions=8&scope=bot");
     }
   }
