8764b7b747af7178d8d5ea6797381972f736c77f
ccans <55026208+ccans@users.noreply.github.com>
2020-05-24 14:36:50 -0500

    lol

---
+++ commands/invite.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+    name: 'img',
+    description: 'Sends an image of a bean',
+    execute(msg, args) {
+      msg.reply("Use this link to add our bot to your server! \n https://discord.com/api/oauth2/authorize?client_id=713881285109743717&permissions=67584&scope=bot");
+    }
+  }
