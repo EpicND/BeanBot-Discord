96d672cd759c7d6614bf8faaa122b0d05766f491
ccans <55026208+ccans@users.noreply.github.com>
2020-05-24 14:38:21 -0500

    ok buddy

--- commands/invite.js
+++ commands/invite.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-    name: 'img',
-    description: 'Sends an image of a bean',
+    name: 'invite',
+    description: 'Invite the bot to your server',
     execute(msg, args) {
       msg.reply("Use this link to add our bot to your server! \n https://discord.com/api/oauth2/authorize?client_id=713881285109743717&permissions=67584&scope=bot");
     }
