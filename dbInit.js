6a67ab2582be702cf0123071c4efd78b19316617
DhairyaG23 <dhairyag23@gmail.com>
2020-05-25 17:55:54 -0500

    added database

--- dbInit.js
+++ dbInit.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('database', 'username', 'password', {
-	host: process.env.DATABASE_URL,
+	host: 'localhost',
 	dialect: 'sqlite',
 	logging: false,
 	storage: 'database.sqlite',
