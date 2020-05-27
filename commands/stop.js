bf3763a0eb7d9edc03cf731de6a06e6c200823b2
DhairyaG23 <dhairyag23@gmail.com>
2020-05-27 16:05:41 -0500

    cc

--- commands/stop.js
+++ commands/stop.js
@@ -1,4 +1,10 @@
 const Discord = require('discord.js');
+const firebaseAdmin = require('firebase-admin')
+const serviceAcc = require('../firebase.json');
+
+var app = firebaseAdmin.app();
+
+const database = app.database();
 
 module.exports = {
     name: "stop",
@@ -8,6 +14,10 @@ module.exports = {
         console.log(`connection: ${connection}`)
 
             msg.member.voice.channel.leave();
+            database.ref(`queues/${msg.guild.id}`).update({
+                playing: false,
+                queue: ["empty"],
+            });
 
     }
 
