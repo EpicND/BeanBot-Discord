cb22cab8a5dd370c3ad22422df66c9df9ebf8d9d
ccans <55026208+ccans@users.noreply.github.com>
2020-05-30 22:10:33 -0500

    Update rob.js

--- commands/rob.js
+++ commands/rob.js
@@ -13,28 +13,28 @@ module.exports = {
     name: 'rob',
     description: 'rob a nerd',
     execute(msg, args, guildSize, UID) {
-      // console.log(args);
-      // var ref = db.ref("/Users/" + UID + "/Money");
-      // console.log("/Users/" + UID + "/Money");
-      // ref.once("value", function(snapshot) {
-      // console.log("The value is " + snapshot.val());
-      // if(snapshot.val() == null) {
-      //   var ref = db.ref("/Users");
-      //   ref.update({
-      //     [UID] : {
-      //       "Inventory" : {
-      //         "Nothing" : "am broke"
-      //       },
-      //       "Money" : 10
-      //     },
-      //   });
-      //   msg.reply("Account created: You have " + snapshot.val() + " beans");
-      // } else {
-      //   msg.reply("You have " + snapshot.val() + " beans");
-      // }
-      //
-      // }, function (errorObject) {
-      // console.log("The read failed: " + errorObject.code);
-      // });
+      console.log(args);
+      var ref = db.ref("/Users/" + UID + "/Money");
+      console.log("/Users/" + UID + "/Money");
+      ref.once("value", function(snapshot) {
+      console.log("The value is " + snapshot.val());
+      if(snapshot.val() == null) {
+        var ref = db.ref("/Users");
+        ref.update({
+          [UID] : {
+            "Inventory" : {
+              "Nothing" : "am broke"
+            },
+            "Money" : 10
+          },
+        });
+        msg.reply("Account created: You have " + snapshot.val() + " beans");
+      } else {
+        msg.reply("You have " + snapshot.val() + " beans");
+      }
+
+      }, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+      });
     }
 }
